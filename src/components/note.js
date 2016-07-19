439bb832b1dd9dc05d5895f9d79f53f9d4d93e8f
Manmeet <manmeetsgujral@gmail.com>
2016-07-19 16:00:19 -0400

    Adding a notete component

---
+++ src/components/note.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Note extends Component {
+  constructor(props) {
+    super(props);
+
+    // init component state here
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Note;
